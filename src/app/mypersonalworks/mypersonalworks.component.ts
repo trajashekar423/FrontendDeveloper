7b6401265be03d16dadf8120b7696aec7e843e19
trajashekar423 <65588319+trajashekar423@users.noreply.github.com>
2024-05-28 12:21:52 +0530

    Update mypersonalworks.component.ts
    logo

--- src/app/mypersonalworks/mypersonalworks.component.ts
+++ src/app/mypersonalworks/mypersonalworks.component.ts
@@ -13,7 +13,7 @@ export class MypersonalworksComponent {
   cards = [
     { title: 'Nextjs/Redux', content: 'My first Nextjs Project with Reduxtoolkit store demo',url:"https://nextjsreduxstore-demo.vercel.app/",logo:"assets/nextjslogo.png" },
     { title: 'Nextjs/Redux', content: 'My Demo project Nextjs with Reduxtoolkit store',url:"https://nextjswithredux-fulltemplate.vercel.app/",logo:"assets/nextjslogo.png" },
-    { title: 'Reactjs/Todolist', content: 'Reactjs Todolist',url:"https://seemly-act.surge.sh/",logo:"assets/nextjslogo.png" },
+    { title: 'Reactjs/Todolist', content: 'Reactjs Todolist',url:"https://seemly-act.surge.sh/",logo:"assets/reactjs.png" },
   ];
 
   constructor() { }
