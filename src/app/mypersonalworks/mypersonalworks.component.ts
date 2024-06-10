66fe28e1517848ce8e3d79debe5d71601f4fdeb3
trajashekar423 <trajashekar423@gmail.com>
2024-06-10 13:24:10 +0530

    new projects added

--- src/app/mypersonalworks/mypersonalworks.component.ts
+++ src/app/mypersonalworks/mypersonalworks.component.ts
@@ -14,6 +14,9 @@ export class MypersonalworksComponent {
     { title: 'Nextjs/Redux', content: 'My first Nextjs Project with Reduxtoolkit store demo',url:"https://nextjsreduxstore-demo.vercel.app/",logo:"assets/nextjslogo.png" },
     { title: 'Nextjs/Redux', content: 'My Demo project Nextjs with Reduxtoolkit store',url:"https://nextjswithredux-fulltemplate.vercel.app/",logo:"assets/nextjslogo.png" },
     { title: 'Reactjs/Todolist', content: 'Reactjs Todolist',url:"https://seemly-act.surge.sh/",logo:"assets/reactjs.png" },
+    { title: 'Reactjs/Addtocart', content: 'Addtocart Demo project Reactjs with Reduxtoolkit ',url:"https://myfirstaddtocart.vercel.app/",logo:"assets/reactjs.png" },
+    { title: 'Nextjs/Redux', content: 'GL Request Form',url:"https://glrequestform.vercel.app/",logo:"assets/nextjslogo.png" },
+    
   ];
 
   constructor() { }
