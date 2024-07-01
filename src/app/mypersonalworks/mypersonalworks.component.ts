680d22580973a07045fb0025041c8bbc0a7336fd
trajashekar423 <trajashekar423@gmail.com>
2024-07-01 11:44:35 +0530

    new project

--- src/app/mypersonalworks/mypersonalworks.component.ts
+++ src/app/mypersonalworks/mypersonalworks.component.ts
@@ -13,6 +13,7 @@ import { Component } from '@angular/core';
 export class MypersonalworksComponent {
 
   cards = [
+    { title: 'Mars Food Services', content: 'Explore the Mars Foodservices family of brands',url:"https://www.marsfoodservices.com/",logo:"assets/mars.png" },
     { title: 'Nextjs/Redux', content: 'My first Nextjs Project with Reduxtoolkit store demo',url:"https://nextjsreduxstore-demo.vercel.app/",logo:"assets/nextjslogo.png" },
     { title: 'Nextjs/Redux', content: 'My Demo project Nextjs with Reduxtoolkit store',url:"https://nextjswithredux-fulltemplate.vercel.app/",logo:"assets/nextjslogo.png" },
     
@@ -22,12 +23,15 @@ export class MypersonalworksComponent {
     { title: 'Reactjs/Todolist', content: 'Reactjs Todolist',url:"https://seemly-act.surge.sh/",logo:"assets/reactjs.png" },
     { title: 'Task Management', content: 'Task Management webapp',url:"https://www.figma.com/design/ja8uwQg1JxQqHfKEs6RfEn/create-task?node-id=0-1&t=GDdvSwCp1jTPXa0e-0",logo:"assets/figma.png" },
     { title: 'APPL Corporate Website', content: 'Task Management webapp',url:"https://www.figma.com/design/drsmdPQczdH10mX0pv5c5p/APPL-Corporate-Website---Homepage-design?node-id=0-1&t=0YG6QTYISDvK8e2I-0",logo:"assets/figma.png" },
+    { title: 'Welovepets', content: 'Good inward & Outward',url:"assets/welovepets.pdf",logo:"assets/welovepets.png" },
 
     { title: 'Truck Tracker', content: 'Good inward & Outward',url:"https://www.figma.com/design/anGhPcmrqZQBuD7PLwDwKp/truck-tracker-new-template?t=C6llNQqyZQWNrjTq-0",logo:"assets/figma.png" },
 
     { title: 'Pladis', content: 'Cookies',url:"https://www.figma.com/design/pQ1FoW8TzEafxb9tyu736z/pladis?node-id=0-1&t=wYmq7qVLMtoSfoZo-0",logo:"assets/pladis.png" },
     { title: 'House of juice', content: 'Ecommerce Juice shop',url:"https://www.figma.com/design/e2Q7gjzUTRr2wUmj6RoNVe/houseofjuice?node-id=0-1&t=6Sc9S4DDEwF5kfue-1",logo:"assets/juice.png" },
     { title: 'Mars Project Gallery', content: 'Ecommerce Juice shop',url:"https://www.figma.com/design/N30OIYzHMAajw66hgOEwXU/mars-brands?node-id=0-1&t=Gq4PFT3QiWeqkvu4-1",logo:"assets/mars.png" },
+
+    
     
   ];
 
